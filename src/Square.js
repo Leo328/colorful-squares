127d91c9e20875d03a427cd5ffee08a44f9c26e2
Matt Lane <lane.matthew@gmail.com>
2018-04-13 14:29:44 -0700

    moving CSS and using stateless functional component

--- src/Square.js
+++ src/Square.js
@@ -1,20 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
+import "./Square.css";
 
-class Square extends Component {
-  render() {
-    console.log("rendering a square~");
-    return (
-      <div
-        style={{
-          width: "100px",
-          height: "100px",
-          backgroundColor: this.props.color,
-          display: "inline-block"
-        }}
-        onClick={this.props.handleClick}
-      />
-    );
-  }
-}
+const Square = ({ color, handleClick }) => (
+  <div
+    className="Square"
+    style={{
+      backgroundColor: color
+    }}
+    onClick={handleClick}
+  />
+);
 
 export default Square;
